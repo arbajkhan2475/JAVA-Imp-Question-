a69e840ecbd32e87d17032d87cb374279c8c5d52
arbaj <ak9179568082@gmail.com>
2025-04-28 22:16:30 +0530

    imp of Java

---
+++ first.java
@@ -0,0 +1,12 @@
+// Q1). Hello World without Semicolon
+
+public class first {
+
+    public static void main(String[] args) {
+
+        if (System.out.printf("Hello World") == null) {
+
+        }
+
+    }
+}
